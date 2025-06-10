23c96ef979e3a162a3a7900c94426ba507d3c43e
wallaceeBenites <wallacepuck@gmail.com>
2025-06-09 22:07:32 -0300

    teste 2

--- read_encoder.v
+++ read_encoder.v
@@ -10,33 +10,27 @@ module Read_Encoder (
     
     reg [1:0] prev_ab;
     
-    
     always @(posedge clk or negedge rst_n) begin
         if (!rst_n) begin
- 
             prev_ab <= 2'b00;
-            dir <= 2'b00; 
         end else begin
-
-            case ({prev_ab, A, B})
-             
-                4'b0010: dir <= 2'b01; 
-                4'b1011: dir <= 2'b01; 
-                4'b1101: dir <= 2'b01; 
-                4'b0100: dir <= 2'b01; 
-                
-            
-                4'b0001: dir <= 2'b10; 
-                4'b0111: dir <= 2'b10; 
-                4'b1110: dir <= 2'b10; 
-                4'b1000: dir <= 2'b10; 
-                
-                
-                default: dir <= 2'b00;
-            endcase
-            
-           
             prev_ab <= {A, B};
         end
+    end
+
+    always @(*) begin 
+        case ({prev_ab, A, B})
+            4'b0010: dir = 2'b01; 
+            4'b1011: dir = 2'b01; 
+            4'b1101: dir = 2'b01; 
+            4'b0100: dir = 2'b01; 
+            
+            4'b0001: dir = 2'b10; 
+            4'b0111: dir = 2'b10; 
+            4'b1110: dir = 2'b10; 
+            4'b1000: dir = 2'b10; 
+            
+            default: dir = 2'b00;
+        endcase
     end
 endmodule
